d4855c75d0a17569322eed69ec4e9f4585b5321b
Guilherme Luiz <guima.luiz@hotmail.com>
2022-04-27 17:35:30 -0300

    Entity Products implementation

--- src/main/java/com/example/course/entities/Product.java
+++ src/main/java/com/example/course/entities/Product.java
@@ -12,46 +12,91 @@ import javax.persistence.Table;
 import javax.persistence.Transient;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
+@Table(name = "tb_product")
+public class Product implements Serializable{
+
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	private String description;
+	private Double price;
+	private String imbUrl;
 	
+	//Para não ter produto com 2 vezes a mesma categoria e instanciamos para garantir que a collection não comece null
 	@Transient
-	private Set<Product> products = new HashSet<>();
+	private Set<Category> categories = new HashSet<>();
 	
-	public Category() {
+	
+	public Product() {
 		
 	}
 
-	public Category(Long id, String name) {
+
+	public Product(Long id, String name, String description, Double price, String imbUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imbUrl = imbUrl;
 	}
+
 
 	public Long getId() {
 		return id;
 	}
 
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 
 	public String getName() {
 		return name;
 	}
 
+
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	public Set<Product> getProducts() {
-		return products;
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public Double getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+
+	public String getImbUrl() {
+		return imbUrl;
+	}
+
+
+	public void setImbUrl(String imbUrl) {
+		this.imbUrl = imbUrl;
+	}
+
+
+	public Set<Category> getCategories() {
+		return categories;
 	}
 
 
@@ -63,6 +108,7 @@ public class Category implements Serializable {
 		return result;
 	}
 
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -71,7 +117,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -79,8 +125,6 @@ public class Category implements Serializable {
 			return false;
 		return true;
 	}
-
-
 	
 	
 	
